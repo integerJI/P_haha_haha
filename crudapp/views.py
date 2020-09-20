b88e5789d88ea80f8415a70ad16f566495f738ed
integerJI <jjs9536@gmail.com>
2020-09-20 22:36:56 +0900

    [Test] github repositoriesList 가져오기

--- crudapp/views.py
+++ crudapp/views.py
@@ -3,9 +3,18 @@ from .models import Blog
 import requests
 from bs4 import BeautifulSoup
 
-source = requests.get('https://www.daum.net/').text
-soup = BeautifulSoup(source, 'html.parser')
-keyword= soup.select('span.txt_ranking')
+githubId = input('아이디를 입력하세요 => ')
+url = 'https://github.com/{}?tab=repositories'.format(githubId)
+req = requests.get(url)
+soup = BeautifulSoup(req.text, 'html.parser')
+
+repositoriesList = soup.select('#user-repositories-list > ul')[0]
+for repository in repositoriesList:
+    repoName = repository.find('a')
+    try:
+        print(repoName.text, end='')
+    except:
+        pass
 
 index = 0
 
@@ -21,6 +30,8 @@ def gogo(request):
     print('gogo 호출')
     
     for key in keyword:
+        key = key.find('a')
+
         print(key.text)
 
     return render(request, 'test.html')
